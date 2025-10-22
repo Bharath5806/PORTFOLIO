4de951b6ff9d6cbe39f368b5e3cf9712abb0119f
Bharath5806 <bharaththokala5806@gmail.com>
2025-10-22 17:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const name = "BHARATH THOKALA";
+const h1 = document.getElementById('name');
+h1.innerHTML = '<span class="cursor"></span>';
+let i = 0;
 
+function type() {
+  if (i < name.length) {
+    h1.innerHTML = name.substring(0, i + 1) + '<span class="cursor"></span>';
+    i++;
+    setTimeout(type, 150);
+  }
+}
+
+setTimeout(type, 800);
